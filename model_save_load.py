c88d77faf4e06b3ada1031df53344ae2bde72770
Pavandev18-Ops <pavankumarj.dev@gmail.com>
2024-11-22 19:58:30 +0530

    model_save_load.py

--- model_save_load.py
+++ model_save_load.py
@@ -1,5 +1,5 @@
 #model_save_load.py
-import joblib
+import pickle
 
 # Function to save both the model and feature names together
 def save_model(model, feature_names, file_path):
@@ -9,12 +9,12 @@ def save_model(model, feature_names, file_path):
         'feature_names': feature_names
     }
     # Save the dictionary as a .pkl file
-    joblib.dump(model_info, file_path)
+    pickle.dump(model_info, file_path)
 
 # Function to load the model and feature names from a file
 def load_model(file_path):
     # Load the saved model and feature names dictionary
-    model_info = joblib.load(file_path)
+    model_info = pickle.load(file_path)
     model = model_info['model']
     feature_names = model_info['feature_names']
     return model, feature_names
