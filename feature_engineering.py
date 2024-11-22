b6ce864ec12be66f4c31a5b50f1e6847d4f9af86
Pavandev18-Ops <pavankumarj.dev@gmail.com>
2024-11-22 17:03:46 +0530

    feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -3,7 +3,6 @@
 import pandas as pd
 
 def create_new_features(df):
-    df['age_adherence_interaction'] = df['age'] * df['adherence_rate']
     df['dosage_deviation'] = (df['dosage'] - df['standard_dosage']) / df['standard_dosage']
     df['treatment_compliance_score'] = df['adherence_rate'] * df['duration_days'] / df['duration_days'].max()
     return df
