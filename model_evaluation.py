7af23f305cb6f181b67c836a1dd73a18dcad4af1
Pavandev18-Ops <pavankumarj.dev@gmail.com>
2024-11-22 17:04:55 +0530

    model_evaluation.py

--- model_evaluation.py
+++ model_evaluation.py
@@ -5,6 +5,5 @@ from sklearn.metrics import mean_squared_error, r2_score
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
     r2 = r2_score(y_test, y_pred)
-    return mse, rmse, r2
+    return mse, r2
