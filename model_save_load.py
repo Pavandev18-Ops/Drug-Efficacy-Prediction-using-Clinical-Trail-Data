83e58f8020bbef6252ad640a3e6a216374298d7d
Pavandev18-Ops <pavankumarj.dev@gmail.com>
2024-11-22 21:34:15 +0530

    model_save_load.py

--- model_save_load.py
+++ model_save_load.py
@@ -1,4 +1,3 @@
-#model_save_load.py
 import pickle
 
 # Function to save both the model and feature names together
@@ -11,12 +10,14 @@ def save_model(model, feature_names, file_path):
     # Save the dictionary as a .pkl file
     with open(file_path, 'wb') as file:  # Open file in write-binary mode
         pickle.dump(model_info, file)
+    print("Model and feature names saved successfully!")
 
 # Function to load the model and feature names from a file
-def load_model(file_path):
+def load_model(model_path):
     # Load the saved model and feature names dictionary
-    with open(file_path, 'rb') as file:  # Open file in read-binary mode
+    with open(model_path, 'rb') as file:  # Open file in read-binary mode
         model_info = pickle.load(file)
+        
     model = model_info['model']
     feature_names = model_info['feature_names']
     return model, feature_names
